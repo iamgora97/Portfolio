a3fe2727de4d390591c940eb3f6bb97dcb2a2766
iamgora97 <gourobnandi28@gmail.com>
2024-07-15 11:06:51 +0530

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import '../App.css'
+import React from 'react';
+import '../App.css';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoMdMail  } from "react-icons/io";
+import { IoMdMail } from "react-icons/io";
 
 const Header = () => {
   return (
-    <div className='flex justify-between pb-5 pt-4 pl-10 pr-10 sticky top-0 z-50000 bg-[#111111]' style={{ boxShadow: '0 20px 70px -10px rgba(231,240,220,0.05)'}}>
+    <div className='flex flex-col md:flex-row justify-between items-center pb-5 pt-4 px-10 sticky top-0 z-50 bg-[#111111]' style={{ boxShadow: '0 20px 70px -10px rgba(231,240,220,0.05)'}}>
+      <div className='text-2xl md:text-4xl text-[#B5C18E] font-extrabold pt-3 md:pt-0'>
+        Gourob Nandi
+      </div>
 
-        <div className='text-4xl text-[#B5C18E] font-extrabold pt-3'>Gourob Nandi</div>
-
-
-
-        <div className='flex justify-between'>
-
-        <div className='pt-4 pr-12 cursor-pointer flex gap-4'>
-        <a href="https://github.com/iamgora97" target='_blank' rel="noreferrer"><FaGithub className='w-5 h-5 text-[#B6C7AA] hover:text-[#B5C18E]'/></a>
-        <a href="https://www.linkedin.com/in/gourob-nandi/" target='_blank' rel="noreferrer"><FaLinkedin className='w-5 h-5 text-[#B6C7AA] hover:text-[#B5C18E]'/></a>
-        <a href="mailto:gourobnandi28@gmail.com" target='_blank' rel="noreferrer"><IoMdMail  className='w-6 h-6 pb-1 text-[#B6C7AA] hover:text-[#B5C18E]'/></a>
+      <div className='flex flex-col md:flex-row items-center md:justify-between'>
+        <div className='flex gap-4 pt-4 md:pt-0'>
+          <a href="https://github.com/iamgora97" target='_blank' rel="noreferrer">
+            <FaGithub className='w-5 h-5 text-[#B6C7AA] hover:text-[#B5C18E]'/>
+          </a>
+          <a href="https://www.linkedin.com/in/gourob-nandi/" target='_blank' rel="noreferrer">
+            <FaLinkedin className='w-5 h-5 text-[#B6C7AA] hover:text-[#B5C18E]'/>
+          </a>
+          <a href="mailto:gourobnandi28@gmail.com" target='_blank' rel="noreferrer">
+            <IoMdMail className='w-6 h-6 pb-1 text-[#B6C7AA] hover:text-[#B5C18E]'/>
+          </a>
         </div>
 
-
-        <button> 
-            <a href="https://bit.ly/Gourob_Nandi" target="_blank" rel="noreferrer" class="btn2" id='button2'><span class="spn2">Resume</span></a>
+        <button className='mt-4 md:mt-0 md:ml-12'>
+          <a href="https://bit.ly/Gourob_Nandi" target="_blank" rel="noreferrer" className="btn2" id='button2'>
+            <span className="spn2">Resume</span>
+          </a>
         </button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
