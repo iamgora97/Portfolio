ce361cb9bedbe0cdaecf246e689ee1c4aa3c31c4
iamgora97 <gourobnandi28@gmail.com>
2024-07-15 11:33:09 +0530

    Update

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -10,12 +10,12 @@ const Certifications = () => {
           </h2>
           
 
-        <p className='text-gray-400 font-medium text-left pt-20 pb-5'>Java - <a href="https://www.hackerrank.com/certificates/9e016f17761e" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Java(Basics) </a> HackerRank</p>
-        <p className='text-gray-400 font-medium text-left pb-5'>Java - <a href="https://www.coursera.org/account/accomplishments/certificate/D8EB5GYFCLM7" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Java Programming: Solving Problems with Software </a> Coursera, Duke University</p>
-        <p className='text-gray-400 font-medium text-left pb-5'>Python - <a href="https://www.hackerrank.com/certificates/f46b03a0befd" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Python(Basics) </a> HackerRank</p>
-        <p className='text-gray-400 font-medium text-left pb-5'>Problem Solving - <a href="https://www.hackerrank.com/certificates/bdea96ebec63" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Problem Solving(Basics) </a> HackerRank</p>
-        <p className='text-gray-400 font-medium text-left pb-5'>CSS - <a href="https://www.hackerrank.com/certificates/19e1492ed8c3" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> CSS(Basics) </a> HackerRank</p>
-        <p className='text-gray-400 font-medium text-left pb-5'>Programming Foundations - <a href="https://coursera.org/share/2d42d7949d4c8fe271d3861eeadf4d46" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Programming Foundations with JavaScript, HTML and CSS </a> Coursera, Duke University</p>
+        <p className='text-gray-400 font-medium text-left pt-20 pb-5'>Java - <a href="https://www.hackerrank.com/certificates/9e016f17761e" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Java(Basics), </a> HackerRank</p>
+        <p className='text-gray-400 font-medium text-left pb-5'>Java - <a href="https://www.coursera.org/account/accomplishments/certificate/D8EB5GYFCLM7" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Java Programming: Solving Problems with Software, </a> Coursera, Duke University</p>
+        <p className='text-gray-400 font-medium text-left pb-5'>Python - <a href="https://www.hackerrank.com/certificates/f46b03a0befd" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Python(Basics), </a> HackerRank</p>
+        <p className='text-gray-400 font-medium text-left pb-5'>Problem Solving - <a href="https://www.hackerrank.com/certificates/bdea96ebec63" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Problem Solving(Basics), </a> HackerRank</p>
+        <p className='text-gray-400 font-medium text-left pb-5'>CSS - <a href="https://www.hackerrank.com/certificates/19e1492ed8c3" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> CSS(Basics), </a> HackerRank</p>
+        <p className='text-gray-400 font-medium text-left pb-5'>Programming Foundations - <a href="https://coursera.org/share/2d42d7949d4c8fe271d3861eeadf4d46" target='_blank' rel="noreferrer" className='text-[#B5C18E]'> Programming Foundations with JavaScript, HTML and CSS, </a> Coursera, Duke University</p>
     </div>
   )
 }
