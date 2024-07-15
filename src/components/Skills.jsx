f17c3f2b93546bb47c125d385997f36133c4d427
iamgora97 <gourobnandi28@gmail.com>
2024-07-15 11:43:32 +0530

    Update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
+import React from 'react';
 
 const Skills = () => {
   return (
-    <div className='mx-10 pt-10'>
-        <h2 className='text-2xl font-semibold flex items-center'>
-        <span className='text-2xl text-[#B5C18E] mr-2'>Skills</span>
+    <div className='mx-4 sm:mx-10 pt-10'>
+      <h2 className='text-xl sm:text-2xl font-semibold flex items-center'>
+        <span className='text-xl sm:text-2xl text-[#B5C18E] mr-2'>Skills</span>
         <span className='hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-slate-700 ml-6'></span>
-        </h2>
-        <div className='grid grid-cols-6 gap-y-2 text-gray-400 pt-8 font-semibold'>
-            
-                <p>Functional Testing</p>
-                <p>End-to-End Testing</p>
-                <p>Software Testing</p>
-                <p>UAT Testing</p>
-                <p>QAT Testing</p>
-                <p>OAT Testing</p>
-                <p>Manual Testing</p>
-                <p>Test Data Preparation</p>
-                <p>Test Case Writing</p>
-                <p>Defect Management</p>
-                <p>Change Request Management</p>
-                <p>QA Documentation</p>
-                <p>Automation Testing</p>
-                <p>Test Case Writing</p>
-                <p>Selenium</p>
-                <>SQL</>
-                <p>MySQL</p>
-                <p>Toad for MySQL</p>
-                <p>MS Office</p>
-                <p>WinSCP</p>
-                <p>Teamforge</p>
-                <p>Eclipse</p>
-                <p>Git</p>
-                <p>Java</p>
-                <p>Python</p>
-                <p>C++</p>
-                <p>HTML</p>
-                <p>CSS</p>
-                <>JavaScript</>
-
-            
-        </div>
+      </h2>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-y-2 text-gray-400 pt-8 font-semibold'>
+        <p>Functional Testing</p>
+        <p>End-to-End Testing</p>
+        <p>Software Testing</p>
+        <p>UAT Testing</p>
+        <p>QAT Testing</p>
+        <p>OAT Testing</p>
+        <p>Manual Testing</p>
+        <p>Test Data Preparation</p>
+        <p>Test Case Writing</p>
+        <p>Defect Management</p>
+        <p>Change Request Management</p>
+        <p>QA Documentation</p>
+        <p>Automation Testing</p>
+        <p>Test Case Writing</p>
+        <p>Selenium</p>
+        <p>SQL</p>
+        <p>MySQL</p>
+        <p>Toad for MySQL</p>
+        <p>MS Office</p>
+        <p>WinSCP</p>
+        <p>Teamforge</p>
+        <p>Eclipse</p>
+        <p>Git</p>
+        <p>Java</p>
+        <p>Python</p>
+        <p>C++</p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
